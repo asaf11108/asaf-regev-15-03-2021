77e1cc2b8d0f13fd41b5998080e89afa070c56e3
asafHerolo <61415155+asafHerolo@users.noreply.github.com>
2021-03-15 17:43:44 +0200

    test5

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.scss';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
+import Button from '@material-ui/core/Button';
+// import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,6 +10,10 @@ function App() {
       <div className="App">
         <div className="content">
           <h1>App component</h1>
+          <Button variant="contained" color="primary">
+            Hello World
+          </Button>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </div>
       </div>
     </ThemeProvider>
